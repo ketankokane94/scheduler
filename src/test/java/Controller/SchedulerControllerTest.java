64d3631267bc06f73d6810e949821192bb4a376a
ketan kokane <ketankokane94@gmail.com>
2020-03-10 18:19:38 -0400

    Removed the body of the controller test function

--- src/test/java/Controller/SchedulerControllerTest.java
+++ src/test/java/Controller/SchedulerControllerTest.java
@@ -18,33 +18,33 @@ public class SchedulerControllerTest {
 
     @Test
     public void mainTest() {
-        ConnectionService connectionService = new ConnectionService();
-        CalendarService taskService = new CalendarService();
-        TasksService projectService = new TasksService();
-//
-        Calendar calendarApp = null;
-        try {
-            calendarApp = connectionService.getCalendarApp();
-        } catch (GeneralSecurityException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        Tasks tasksApp = null;
-        try {
-            tasksApp = connectionService.getTaskApp();
-        } catch (GeneralSecurityException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-//
-//        final List<Interval> intervals = taskService.pullIntervals(calendarApp);
-//        System.out.println(intervals);
-        final List<Project> projects = projectService.pullTasks(tasksApp);
-        System.out.println(projects);
-        //
-//        final List<Interval> eventList = new SchedulerService().run(intervals,projects, true);
-//        taskService.pushTasks(eventList, calendarApp);
+//        ConnectionService connectionService = new ConnectionService();
+//        CalendarService taskService = new CalendarService();
+//        TasksService projectService = new TasksService();
+////
+//        Calendar calendarApp = null;
+//        try {
+//            calendarApp = connectionService.getCalendarApp();
+//        } catch (GeneralSecurityException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        Tasks tasksApp = null;
+//        try {
+//            tasksApp = connectionService.getTaskApp();
+//        } catch (GeneralSecurityException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+////
+////        final List<Interval> intervals = taskService.pullIntervals(calendarApp);
+////        System.out.println(intervals);
+//        final List<Project> projects = projectService.pullTasks(tasksApp);
+//        System.out.println(projects);
+//        //
+////        final List<Interval> eventList = new SchedulerService().run(intervals,projects, true);
+////        taskService.pushTasks(eventList, calendarApp);
     }
 }
